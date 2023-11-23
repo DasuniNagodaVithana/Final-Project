ac5c11fc573c9d6ed5cea7a63f6e9123191cb3c0
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2023-11-23 19:11:25 +0530

    Create and initiate the onboard Screen
    I created a folder in the screens folder. then create a tsx file for the onboard screen to develop it.

---
+++ src/screens/OnBoardScreen/OnBoardScreen.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+
+const OnBoardScreen  = (props: OnBoardScreen Props) => {
+  return (
+    <View style={styles.container}>
+      <Text>OnBoardScreen 
+    
+      </Text>
+    </View>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default OnBoardScreen ;
