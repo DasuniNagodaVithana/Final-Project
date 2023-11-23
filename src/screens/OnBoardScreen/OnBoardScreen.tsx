9c4dd287c2c5d412c53e696791f45a0326d0dc23
Suvini Nimthara <129636274+suvininimthara@users.noreply.github.com>
2023-11-23 19:29:04 +0530

    Setup onboardscreen
    I set up the onboard screen and add fonts and images

--- src/screens/OnBoardScreen/OnBoardScreen.tsx
+++ src/screens/OnBoardScreen/OnBoardScreen.tsx
@@ -1,22 +1,64 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, ImageBackground} from 'react-native';
 
 
 
-const OnBoardScreen  = (props: OnBoardScreen Props) => {
+
+const OnBoardScreen: React.FunctionComponent = () => {
   return (
-    <View style={styles.container}>
-      <Text>OnBoardScreen 
-    
-      </Text>
-    </View>
+    <ImageBackground
+    source={require('../assessts/images/coast-3384845.jpg')}
+    style={styles.profileImage}
+
+    >
+    <View style={styles.container}>  
+            <Text style={styles.welcomeText}>Explore Sri Lanka with Us</Text>  
+            <View style={styles.button}>
+                <TouchableOpacity>
+                    <Text style={styles.buttonText}>Let's Go</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </ImageBackground>
   );
 };
 
 
 
 const styles = StyleSheet.create({
-  container: {}
+    container: {
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginBottom: 50,
+      },
+        profileImage: {
+            width: '100%',
+            height: '100%',
+            resizeMode: 'cover',
+        },
+        button: {
+            backgroundColor: '#359CBB',
+            width: 200,
+            height: 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 20,
+        },
+        buttonText: {
+            color: '#fff',
+            fontSize: 20,
+            fontFamily: 'Outfit-Bold',
+        },
+        welcomeText: {
+            justifyContent: 'center',
+            color: '#fff',
+            fontSize: 35,
+            fontFamily: 'Urbanist-Bold',
+            textAlign: 'center',
+            padding: 30,
+        },
 });
 
 export default OnBoardScreen ;
