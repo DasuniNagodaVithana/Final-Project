45bd5b3a6861e64d034946929841ea560fedc921
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2023-11-28 06:20:23 +0530

    firebase work -test1

--- index.js
+++ index.js
@@ -5,5 +5,42 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { firebase } from '@react-native-firebase/app';
+
+const firebaseConfig = {
+ 
+    "project_info": {
+      "project_number": "656423335364",
+      "project_id": "auth-e308d",
+      "storage_bucket": "auth-e308d.appspot.com"
+    },
+    "client": [
+      {
+        "client_info": {
+          "mobilesdk_app_id": "1:656423335364:android:3117a58513ebc13166e091",
+          "android_client_info": {
+            "package_name": "com.authentication"
+          }
+        },
+        "oauth_client": [],
+        "api_key": [
+          {
+            "current_key": "AIzaSyD1to6NJvkPLdWWtTvdKrMLMgHHGLw8G1c"
+          }
+        ],
+        "services": {
+          "appinvite_service": {
+            "other_platform_oauth_client": []
+          }
+        }
+      }
+    ],
+    "configuration_version": "1"
+  
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 AppRegistry.registerComponent(appName, () => App);
