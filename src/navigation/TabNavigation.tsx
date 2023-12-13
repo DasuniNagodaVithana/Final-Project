1874f92699bf2a7d61a5387bc9c413bbd9ee65d7
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2023-12-13 19:23:51 +0530

    settingup gesture handler and bottomtab navigation

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Image, StyleSheet, Text, View } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 import Favourite from "../screens/HomeScreen/Favourite";
 import Search from "../screens/HomeScreen/Search";
-import StackNavigator from "../screens/StackNavigator";
 import Index from "../screens/HomeScreen";
 import MapScreen from "../screens/HomeScreen/MapScreen";
 import UserProfile from "../screens/UserProfile/UserProfile";
 
 const Tab = createBottomTabNavigator();
 
+const CustomTabBarButton = ({ children }: { children: ReactNode }) => {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    // Navigate to the "Search" screen
+    navigation.navigate("Search");
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View
+        style={{
+          top: -20,
+          justifyContent: "center",
+          alignItems: "center",
+          ...styles.shadow,
+        }}
+      >
+        <View
+          style={{
+            width: 65,
+            height: 65,
+            borderRadius: 35,
+            backgroundColor: "#e32f45",
+          }}
+        >
+          {children}
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 const TabNavigator: React.FC = () => {
   return (
@@ -22,41 +55,33 @@ const TabNavigator: React.FC = () => {
         tabBarStyle: [
           {
             position: "absolute",
-            bottom: 25,
-            left: 20,
-            right: 20,
-            elevation: 5, 
+            bottom: 1,
+            left: 4,
+            right: 4,
+            elevation: 5,
             backgroundColor: "#ffffff",
-            borderRadius: 15,
-            height: 90,
+            borderRadius: 12,
+            height: 55,
+            
           },
-          styles.shadow, 
+          styles.shadow,
         ],
-        tabBarItemStyle: {
-          // Adjust the icon position for the "Search" tab
-          transform: [{ translateY: -10 }], // You can adjust the value as needed
-        },
       }}
     >
       <Tab.Screen
         name="Home"
         component={Index}
         options={{
-          tabBarIcon: ({focused}) => (
-            <View style={
-              {alignItems:'center',justifyContent:'center',top:10}
-            }>
-            <Image
-              source={require("../assets/images/image12.png")}
-              resizeMode="contain"
-              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
-            />
-            <Text style={{
-              color:focused?'#e32f45':'#748c94',
-              fontSize:12
-            }}>
-              HOME
-            </Text>
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center", top: 4}}>
+              <Image
+                source={require("../assets/images/image12.png")}
+                resizeMode="contain"
+                style={{ width: 25, height: 25, tintColor: focused ? "#e32f45" : "#748c94" }}
+              />
+              <Text style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}>
+                HOME
+              </Text>
             </View>
           ),
         }}
@@ -65,21 +90,16 @@ const TabNavigator: React.FC = () => {
         name="Favourite"
         component={Favourite}
         options={{
-          tabBarIcon: ({focused}) => (
-            <View style={
-              {alignItems:'center',justifyContent:'center',top:10}
-            }>
-            <Image
-              source={require("../assets/images/w1.png")}
-              resizeMode="contain"
-              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
-            />
-            <Text style={{
-              color:focused?'#e32f45':'#748c94',
-              fontSize:12
-            }}>
-              WEATHER
-            </Text>
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center", top: 4}}>
+              <Image
+                source={require("../assets/images/w1.png")}
+                resizeMode="contain"
+                style={{ width: 26, height: 26, tintColor: focused ? "#e32f45" : "#748c94" }}
+              />
+              <Text style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}>
+                WEATHER
+              </Text>
             </View>
           ),
         }}
@@ -88,44 +108,30 @@ const TabNavigator: React.FC = () => {
         name="Search"
         component={Search}
         options={{
-          tabBarIcon: ({focused}) => (
-            <View style={
-              {alignItems:'center',justifyContent:'center',top:10}
-            }>
+          tabBarIcon: () => (
             <Image
               source={require("../assets/images/image20.png")}
               resizeMode="contain"
-              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
+              style={{ width: 50, height: 50, tintColor: "#fff" }}
             />
-            <Text style={{
-              color:focused?'#e32f45':'#748c94',
-              fontSize:12
-            }}>
-              FIND
-            </Text>
-            </View>
           ),
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
         }}
       />
       <Tab.Screen
         name="Map"
         component={MapScreen}
         options={{
-          tabBarIcon: ({focused}) => (
-            <View style={
-              {alignItems:'center',justifyContent:'center',top:10}
-            }>
-            <Image
-              source={require("../assets/images/image13.png")}
-              resizeMode="contain"
-              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
-            />
-            <Text style={{
-              color:focused?'#e32f45':'#748c94',
-              fontSize:12
-            }}>
-              MAP
-            </Text>
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center", top: 4 }}>
+              <Image
+                source={require("../assets/images/image13.png")}
+                resizeMode="contain"
+                style={{ width: 25, height: 25, tintColor: focused ? "#e32f45" : "#748c94" }}
+              />
+              <Text style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}>
+                MAP
+              </Text>
             </View>
           ),
         }}
@@ -134,21 +140,16 @@ const TabNavigator: React.FC = () => {
         name="User"
         component={UserProfile}
         options={{
-          tabBarIcon: ({focused}) => (
-            <View style={
-              {alignItems:'center',justifyContent:'center',top:10}
-            }>
-            <Image
-              source={require("../assets/images/user1.png")}
-              resizeMode="contain"
-              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
-            />
-            <Text style={{
-              color:focused?'#e32f45':'#748c94',
-              fontSize:12
-            }}>
-              USER
-            </Text>
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center", top: 4 }}>
+              <Image
+                source={require("../assets/images/user1.png")}
+                resizeMode="contain"
+                style={{ width: 25, height: 25, tintColor: focused ? "#e32f45" : "#748c94" }}
+              />
+              <Text style={{ color: focused ? "#e32f45" : "#748c94", fontSize: 12 }}>
+                USER
+              </Text>
             </View>
           ),
         }}
