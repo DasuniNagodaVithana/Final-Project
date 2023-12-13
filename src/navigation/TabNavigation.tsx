49a066275f3c6ab484758c589d81fe2eebc15449
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2023-12-13 15:43:55 +0530

    customize Tab bar

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Image } from "react-native"; // Import Image component
+import { Image, StyleSheet, Text, View } from "react-native";
 
 import Favourite from "../screens/HomeScreen/Favourite";
 import Search from "../screens/HomeScreen/Search";
@@ -8,36 +8,55 @@ import Index from "../screens/HomeScreen";
 import MapScreen from "../screens/HomeScreen/MapScreen";
 import UserProfile from "../screens/UserProfile/UserProfile";
 
-
 const Tab = createBottomTabNavigator();
+
 
 const TabNavigator: React.FC = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={{ headerShown: false, tabBarShowLabel: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: [
+          {
+            position: "absolute",
+            bottom: 25,
+            left: 20,
+            right: 20,
+            elevation: 5, 
+            backgroundColor: "#ffffff",
+            borderRadius: 15,
+            height: 90,
+          },
+          styles.shadow, 
+        ],
+        tabBarItemStyle: {
+          // Adjust the icon position for the "Search" tab
+          transform: [{ translateY: -10 }], // You can adjust the value as needed
+        },
+      }}
     >
       <Tab.Screen
         name="Home"
         component={Index}
         options={{
-          tabBarIcon: () => (
+          tabBarIcon: ({focused}) => (
+            <View style={
+              {alignItems:'center',justifyContent:'center',top:10}
+            }>
             <Image
               source={require("../assets/images/image12.png")}
-              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
             />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={Search}
-        options={{
-          tabBarIcon: () => (
-            <Image
-              source={require("../assets/images/image20.png")}
-              style={{ width:40, height: 40}}
-            />
+            <Text style={{
+              color:focused?'#e32f45':'#748c94',
+              fontSize:12
+            }}>
+              HOME
+            </Text>
+            </View>
           ),
         }}
       />
@@ -45,11 +64,45 @@ const TabNavigator: React.FC = () => {
         name="Favourite"
         component={Favourite}
         options={{
-          tabBarIcon: () => (
+          tabBarIcon: ({focused}) => (
+            <View style={
+              {alignItems:'center',justifyContent:'center',top:10}
+            }>
             <Image
               source={require("../assets/images/w1.png")}
-              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
             />
+            <Text style={{
+              color:focused?'#e32f45':'#748c94',
+              fontSize:12
+            }}>
+              WEATHER
+            </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <View style={
+              {alignItems:'center',justifyContent:'center',top:10}
+            }>
+            <Image
+              source={require("../assets/images/image20.png")}
+              resizeMode="contain"
+              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
+            />
+            <Text style={{
+              color:focused?'#e32f45':'#748c94',
+              fontSize:12
+            }}>
+              FIND
+            </Text>
+            </View>
           ),
         }}
       />
@@ -57,11 +110,22 @@ const TabNavigator: React.FC = () => {
         name="Map"
         component={MapScreen}
         options={{
-          tabBarIcon: () => (
+          tabBarIcon: ({focused}) => (
+            <View style={
+              {alignItems:'center',justifyContent:'center',top:10}
+            }>
             <Image
               source={require("../assets/images/image13.png")}
-              style={{ width: 26, height: 26 }}
+              resizeMode="contain"
+              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
             />
+            <Text style={{
+              color:focused?'#e32f45':'#748c94',
+              fontSize:12
+            }}>
+              MAP
+            </Text>
+            </View>
           ),
         }}
       />
@@ -69,16 +133,40 @@ const TabNavigator: React.FC = () => {
         name="User"
         component={UserProfile}
         options={{
-          tabBarIcon: () => (
+          tabBarIcon: ({focused}) => (
+            <View style={
+              {alignItems:'center',justifyContent:'center',top:10}
+            }>
             <Image
               source={require("../assets/images/user1.png")}
-              style={{ width: 26, height: 26 }}
+              resizeMode="contain"
+              style={{ width: 24, height: 24,tintColor:focused?'#e32f45':'#748c94', }}
             />
+            <Text style={{
+              color:focused?'#e32f45':'#748c94',
+              fontSize:12
+            }}>
+              USER
+            </Text>
+            </View>
           ),
         }}
       />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+});
 
 export default TabNavigator;
